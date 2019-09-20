5ddcb2325739155129d14f0bf00f3212ac9db1e0
yuhaobin <yuhaobin0816@163.com>
2019-09-20 18:09:09 +0800

    使用dockerfile部署springboot应用

--- src/main/java/com/refactoring/computecorerefactoring/controller/test.java
+++ src/main/java/com/refactoring/computecorerefactoring/controller/test.java
@@ -1,11 +1,13 @@
 package com.refactoring.computecorerefactoring.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class test {
     @RequestMapping("/test")
+    @ResponseBody
     public String get(){
         return "test";
     }
